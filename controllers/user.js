4290f792192cad3f62612ec7ac3b061de292872c
Maryuandi <maryuandidevrizal@gmail.com>
2023-11-01 21:40:14 +0700

    Part 1-9

---
+++ controllers/user.js
@@ -0,0 +1,66 @@
+let users = [ 
+
+    {id: 1, nama: "Devrizal", email: "devrizal@gmail.com"},
+    {id: 2, nama: "Gita", email: "rerereregita@gmail.com"},
+
+]
+
+
+module.exports = {
+    index: (req, res) => {
+     if (users.length > 0) {
+      res.json({
+       status: true,
+       data: users,
+       method: req.method,
+       url: req.url,
+      });
+     } else {
+      res.json({
+       status: false,
+       message: "Data masih kosong",
+      });
+     }
+    },
+   
+    store: (req, res) => {
+     users.push(req.body);
+     res.json({
+      status: true,
+      data: users,
+      method: req.method,
+      url: req.url,
+      message: "Data berhasil ditambahkan",
+     });
+    },
+   
+    update: (req, res) => {
+     const id = req.params.id;
+     users.filter((user) => {
+      if (user.id == id) {
+       user.name = req.body.name;
+       user.email = req.body.email;
+       return user;
+      }
+     });
+     res.json({
+      status: true,
+      data: users,
+      method: req.method,
+      url: req.url,
+      message: "Data berhasil diubah",
+     });
+    },
+    delete: (req, res) => {
+     const id = req.params.id;
+     users = users.filter((user) => user.id != id);
+   
+     res.json({
+      status: true,
+      data: users,
+      method: req.method,
+      url: req.url,
+      message: "Data berhasil dihapus",
+     });
+    },
+   };
